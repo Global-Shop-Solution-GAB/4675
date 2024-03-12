af06bcc73aff59148df6cfd214e496cfaa03cf84
Service Web <sw@gssmail.com>
2024-03-12 12:32:50 -0500

    Commit changes in C:\Websites\Git\emarlow\4675\GCG_4675_LateWOByWC.sql

---
+++ GCG_4675_LateWOByWC.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_4675_LateWOByWC
+ * ========================================== */
+CREATE TABLE "GCG_4675_LateWOByWC"(
+ "BIRunID" INTEGER,
+ "Job" CHAR(6),
+ "Suffix" CHAR(3),
+ "Seq" CHAR(6),
+ "Part" CHAR(20),
+ "Units_Open" NUMERIC(12,4),
+ "WC" CHAR(4),
+ "Description" CHAR(30),
+ "DateJobDue" DATE,
+ "DateSeqDue" DATE);
